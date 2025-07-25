ce160b5032c043d0eef59680ec1aa603164eb36e
Chris Lin <chrislin@nvidia.com>
2025-07-25 02:30:02 +0000

    update scripts

--- finetune/RLBench/train.sh
+++ finetune/RLBench/train.sh
@@ -1,15 +1,14 @@
-cd /PATH_TO_BRIDGEVLA/finetune
+cd finetune
 export COPPELIASIM_ROOT=$(pwd)/CoppeliaSim_Edu_V4_1_0_Ubuntu20_04 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
 export DISPLAY=:1.0
 
-cd /PATH_TO_BRIDGEVLA/finetune/RLBench
+cd RLBench
 
-
-port=MASTER_PORT
-GPUS_PER_NODE=NUMBER_OF_GPU_PER_MACHINE
-NNODES=NUMBER_OF_MACHINE
+port=29501
+GPUS_PER_NODE=1
+NNODES=1
 torchrun \
     --nnodes=$NNODES \
     --nproc_per_node=$GPUS_PER_NODE \
--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-# Pretrain
-bash pretrain/pretrain.sh --branches 2 \
-    --config_path pretrain_config.yaml \
-    --json_detection_path ../outputs/pretrain_data/detection_data.json \
-    --image_folder ../outputs/pretrain_data
+# # Pretrain
+# bash pretrain/pretrain.sh --branches 2 \
+#     --config_path pretrain_config.yaml \
+#     --json_detection_path ../outputs/pretrain_data/detection_data.json \
+#     --image_folder ../outputs/pretrain_data
 
-# # RLBench Finetune
-# bash finetune/RLBench/train.sh --exp_cfg_path  configs/rlbench_config.yaml \
-#     --exp_note rlbench \
-#     --freeze_vision_tower \
-#     --log_dir ../outputs/logs/rlbench \
-#     --load_pretrain \
-#     --pretrain_path  ../outputs/results/pretrain/baseline_bridgevla_pretrain/20250722_150000/checkpoint-1000  
+# RLBench Finetune
+bash finetune/RLBench/train.sh --exp_cfg_path  configs/rlbench_config.yaml \
+    --exp_note rlbench \
+    --freeze_vision_tower \
+    --log_dir ../outputs/logs/rlbench \
+    --load_pretrain \
+    --pretrain_path  ../../outputs/pretrain/baseline_bridgevla_pretrain/20250723_034837/checkpoint-45856  
